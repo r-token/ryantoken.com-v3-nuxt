0d9f773c5c4414fa4df7df4bfcf33af43043d112
Ryan Token <ryantoken13@gmail.com>
2022-07-16 15:42:58 -0600

    Playing with Netlify functions

--- netlify/functions/sentence-generator.js
+++ netlify/functions/sentence-generator.js
@@ -1,6 +1,8 @@
 'use strict'
 
 exports.handler = async function(event, context) {
+	console.log('event:', JSON.stringify(event, null, 4))
+	
 	const nouns = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog"]
 	const verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"]
 	const adjectives = ["beautiful", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"]
@@ -11,7 +13,7 @@ exports.handler = async function(event, context) {
 		return Math.floor(Math.random() * 10)
 	}
 	
-	function sentence() {
+	function generateSentence() {
 		var rand1 = generateRandomNumber()
 		var rand2 = generateRandomNumber()
 		var rand3 = generateRandomNumber()
@@ -19,11 +21,15 @@ exports.handler = async function(event, context) {
 		var rand5 = generateRandomNumber()
 		var rand6 = generateRandomNumber()
 		
-		const content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + prepositions[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + "."
+		const sentence = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + prepositions[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + "."
 		
-		return content
+		return sentence
 	}
 	
-	const randomSentence = sentence()
-	return randomSentence
+	const randomSentence = generateSentence()
+	
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: randomSentence })
+	}
 }
--- pages/experimentation.vue
+++ pages/experimentation.vue
@@ -6,8 +6,8 @@
   const config = useRuntimeConfig()
   const { data: sentence } = await useFetch(`${config.public.apiBase}/sentence-generator`)
   const refresh = () => {
-    console.log('refreshing data')
     refreshNuxtData('sentence')
+    console.log('sentence:', sentence.value)
   }
 </script>
 
@@ -24,7 +24,10 @@
         A Work in Progress...
       </p>
       
-      <p class="mb-4 bg-gradient-to-r from-indigo-500 to-fuchsia-500">{{sentence}}</p>
+      <p class="mb-4 bg-gradient-to-r from-indigo-500 to-fuchsia-500">
+        {{sentence}}
+      </p>
+      
       <button class="rounded-full p-3 bg-indigo-500 dark:bg-sky-500 dark:from-fuchsia-500 dark:to-red-500 text-white" @click="refresh">Generate random sentence</button>
     </div>
   </div>
