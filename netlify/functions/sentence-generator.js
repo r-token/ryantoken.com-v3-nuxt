7c3bfae393b78d07b361880d38c4f40aa946e814
Ryan Token <ryantoken13@gmail.com>
2022-07-17 20:08:28 -0600

    Updated the sentence-generator function and the experimentation page

--- netlify/functions/sentence-generator.js
+++ netlify/functions/sentence-generator.js
@@ -1,14 +1,14 @@
 'use strict'
 
 exports.handler = async function(event, context) {
-	const nouns = ["bird", "clock", "old man", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog"]
-	const verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"]
-	const adjectives = ["handsome", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"]
-	const adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"]
-	const prepositions = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"]
+	const nouns = ["bird", "woman", "man", "gator", "duck", "teacher", "dinosaur", "professor", "hamster", "dog", "dragon", "tiger", "track star"]
+	const verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed", "burned", "ducked", "punched"]
+	const adjectives = ["handsome", "lazy", "professional", "lovely", "sad", "rough", "soft", "hot", "fascinating", "slimy", "mature", "young", "platitudinous"]
+	const adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately", "smoothly", "aggressively", "lazily"]
+	const prepositions = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards", "against", "under", "off"]
 	
 	function generateRandomNumber() {
-		return Math.floor(Math.random() * 10)
+		return Math.floor(Math.random() * 13)
 	}
 	
 	function generateSentence() {
@@ -17,9 +17,8 @@ exports.handler = async function(event, context) {
 		const rand3 = generateRandomNumber()
 		const rand4 = generateRandomNumber()
 		const rand5 = generateRandomNumber()
-		const rand6 = generateRandomNumber()
 		
-		const sentence = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + prepositions[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + "."
+		const sentence = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + prepositions[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + "."
 		
 		return sentence
 	}
--- pages/experimentation.vue
+++ pages/experimentation.vue
@@ -31,16 +31,16 @@
     
     <hr class="mb-6" />
     
-    <div class="cursor-help">
-      <p class="mb-14 font-extrabold break-words text-transparent text-4xl animate-pulse bg-clip-text bg-gradient-to-r from-indigo-500 to-fuchsia-500 dark:from-fuchsia-500 dark:to-red-500">
+    <div class="cursor-help h-screen">
+      <p class="mb-14 font-extrabold break-words text-transparent text-2xl animate-pulse bg-clip-text bg-gradient-to-r from-indigo-500 to-fuchsia-500 dark:from-sky-500 dark:to-green-500">
         A Work in Progress...
       </p>
       
-      <Header text="Generate a random sentence with this simple serverless function:"/>
+      <Header text="Netlify Function:"/>
         
       <button class="mb-6 rounded-full p-3 bg-indigo-500 dark:bg-sky-500 dark:from-fuchsia-500 dark:to-red-500 text-white" @click="refresh">Generate a new random sentence</button>
       
-      <p class="mb-4 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-red-500 dark:from-fuchsia-500 dark:to-red-500">
+      <p class="mb-4 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-red-500 dark:from-sky-500 dark:to-green-500">
         {{sentence()}}
       </p>
     </div>
