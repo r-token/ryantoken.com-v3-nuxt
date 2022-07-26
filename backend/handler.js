e4de2c3db5d1c1eba10090ed6ec92f08f0a6867b
Ryan Token <ryantoken13@gmail.com>
2022-07-25 23:26:02 -0600

    Configured and deployed a serverless API endpoint that returns a signed S3 URL for my resume

---
+++ backend/handler.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+
+const getResumeFromS3 = async (event, context) => {
+  console.log('event:', JSON.stringify(event, null, 2))
+  
+  if (event.source === 'serverless-plugin-warmup') {
+    return 'Lambda is warm!'
+  }
+  
+  const stage = process.env.STAGE
+  const resumeBucket = stage === 'prod' ? 'ryan-token-resume-prod' : 'ryan-token-resume-dev'
+  
+  let response = {
+    statusCode: 200,
+    body: {}
+  }
+  
+  const s3Params = {
+    Bucket: resumeBucket,
+    Key: 'Ryan Token Resume.pdf',
+    Expires: 30
+  }
+  
+  try {
+    const signedUrlForResume = s3.getSignedUrl("getObject", s3Params)
+    response.body = signedUrlForResume
+  } catch(err) {
+    console.error('error getting signed URL for resume:', err)
+    context.captureError(err)
+    response.statusCode = 500
+    response.body = 'error: unable to get signed URL for resume'
+    return response
+  }
+  
+  response.body = JSON.stringify(response.body, null, 2)
+  return response
+}
+
+module.exports = {
+  getResumeFromS3
+}
