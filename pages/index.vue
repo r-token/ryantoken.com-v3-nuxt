5d4fcb630434861b3c34477e697913c60aad8ce7
Ryan Token <ryantoken13@gmail.com>
2022-07-26 10:38:19 -0600

    Debugging a NuxtLink loading issue

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,7 @@
   })
 
   const config = useRuntimeConfig()
-  const { data, pending, error, refresh } = await useFetch(`${config.public.customApiBase}/resume`)
-  
-  const signedUrlForResume = ref('#')
-  if (!pending.value) {
-    signedUrlForResume.value = JSON.parse(data.value)
-  }
+  const { data: signedUrlForResume, refresh } = await useFetch(`${config.public.customApiBase}/resume`)
 
   const recentBlogPosts = await queryContent('/blog')
     .sort({ date: -1 }) // show latest articles first
@@ -25,9 +20,11 @@
     <p class="text-gray-600 dark:text-gray-400 mb-4">
       I'm a <ExternalLink url="https://github.com/r-token" text="software engineer"/>, <ExternalLink url="https://thegoldenhurricast.com/podcast" text="podcaster" />, <NuxtLink class="text-indigo-500 dark:text-sky-300" to="https://ryantoken.com/blog">writer</NuxtLink>, <ExternalLink url="https://thegoldenhurricast.com" text="college sports fan" />, and general techie.
     </p>
+    
+    <p>{{JSON.parse(signedUrlForResume)}}</p>
 
     <p class="text-gray-600 dark:text-gray-400 mb-4">
-      I'm <ExternalLink :to="signedUrlForResume" text="currently" @click="refresh" /> a full-stack software engineer at <ExternalLink url="https://new.trystoryboard.com" text="Storyboard" />. My primary focus there is on <ExternalLink url="https://www.serverless.com/framework" text="serverless web apps" />, but I build software that touches every part of the company.
+      I'm <ExternalLink :to="JSON.parse(signedUrlForResume)" text="currently" @click="refresh" /> a full-stack software engineer at <ExternalLink url="https://new.trystoryboard.com" text="Storyboard" />. My primary focus there is on <ExternalLink url="https://www.serverless.com/framework" text="serverless web apps" />, but I build software that touches every part of the company.
     </p>
     
     <p class="text-gray-600 dark:text-gray-400 mb-6">
