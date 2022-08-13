c866a466355e41773ea479516b23ef00c78f0915
Ryan Token <ryantoken13@gmail.com>
2022-08-13 15:28:02 -0600

    Updated favicons, trying nuxt-img again, and updated to the latest version of nuxt 3 for everything

--- components/content/ResizableBlogImage.vue
+++ components/content/ResizableBlogImage.vue
@@ -8,6 +8,6 @@
 
 <template>
   <NuxtLink :to="url" class="m-auto w-full" target="_blank">
-    <img :src="src" :alt="altText" class="p-2 rounded-2xl" />
+    <nuxt-img :src="src" :alt="altText" class="p-2 rounded-2xl" />
   </NuxtLink>
 </template>
