a55602b43986a2ff13fdd99a1b2e5a0da7181045
Ryan Token <ryan@storyboard.fm>
2022-10-24 12:26:28 -0500

    Blog post updates and Meta page updates

--- pages/meta.vue
+++ pages/meta.vue
@@ -19,5 +19,9 @@
     <p class="mb-4">The serverless functions are built with either the <ExternalLink url="https://www.serverless.com/framework" text="Serverless Framework"/> or <ExternalLink url="https://www.netlify.com/products/functions/" text="Netlify Functions"/>.</p>
 
     <p class="mb-4">The source code and README for this site are on <ExternalLink url="https://github.com/r-token/ryantoken.com-v3" text="GitHub" />.</p>
+    
+    <p class="mb-8">
+      Previous versions of the site were built with <ExternalLink url="https://getbootstrap.com/" text="Bootstrap" />, <ExternalLink url="https://github.com/johnsundell/publish" text="Publish" />, <ExternalLink url="https://www.netlify.com/" text="Netlify" />, and others. I'm <NuxtLink to="/blog/nuxt-3-vs-sveltekit" class="text-indigo-500 dark:text-sky-300">simultaneously</NuxtLink> maintaining a <ExternalLink url="https://www.ryantoken.com" text="SvelteKit version" /> of this website.
+    </p>
   </div>
 </template>
