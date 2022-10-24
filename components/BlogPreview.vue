cc831e0edac89e6d65ae6c17a9da6518ee810e01
Ryan Token <ryan@storyboard.fm>
2022-10-23 23:04:43 -0500

    netlify.toml updates to go back to ssr

--- components/BlogPreview.vue
+++ components/BlogPreview.vue
@@ -28,7 +28,7 @@
     <div class="cursor-pointer" @click="navigate(slug)">
       <div class="md:flex">
         <div class="md:shrink-0 max-w-xs max-h-xs">
-          <nuxt-img class="w-full md:h-full md:w-48" :src="image" :alt="imageAlt" />
+          <img class="w-full md:h-full md:w-48" :src="image" :alt="imageAlt" />
         </div>
 
         <div class="px-8 pt-4 relative">
